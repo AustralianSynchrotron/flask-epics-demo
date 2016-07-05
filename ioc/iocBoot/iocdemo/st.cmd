fc9f93c1b76e44e5282aff7fddfe0928deec5b5c
RobbieClarken <robbie.clarken@gmail.com>
2016-07-05 14:33:07 +1000

    Configure IOC to use sim detector and ffmpegServer

--- ioc/iocBoot/iocdemo/st.cmd
+++ ioc/iocBoot/iocdemo/st.cmd
@@ -1,21 +1,30 @@
 #!../../bin/linux-x86_64/demo
 
-## You may have to change demo to something else
-## everywhere it appears in this file
-
 < envPaths
+epicsEnvSet("PREFIX", "SR00DEMO01:")
+epicsEnvSet("PORT",   "SIM1")
+epicsEnvSet("QSIZE",  "20")
+epicsEnvSet("XSIZE",  "500")
+epicsEnvSet("YSIZE",  "500")
+epicsEnvSet("NCHANS", "2048")
+epicsEnvSet("CBUFFS", "500")
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "20000000")
 
-cd "${TOP}"
-
-## Register all support components
-dbLoadDatabase "dbd/demo.dbd"
+dbLoadDatabase("$(TOP)/dbd/demo.dbd")
 demo_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-#dbLoadRecords("db/xxx.db","user=rootHost")
+simDetectorConfig("$(PORT)", $(XSIZE), $(YSIZE), 1, 0, 0)
+dbLoadRecords("$(ADEXAMPLE)/db/simDetector.template","P=$(PREFIX),R=CAM1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
-cd "${TOP}/iocBoot/${IOC}"
-iocInit
+ffmpegServerConfigure(8000)
+ffmpegStreamConfigure("MPEG1", 2, 0, "$(PORT)", "0", -1)
+dbLoadRecords("$(FFMPEGSERVER)/db/ffmpegStream.template", "P=$(PREFIX),R=MPEG1:,PORT=MPEG1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT)")
 
-## Start any sequence programs
-#seq sncxxx,"user=rootHost"
+iocInit()
+
+epicsThreadSleep(1)
+
+dbpf("$(PREFIX)CAM1:Acquire", "Acquire")
+dbpf("$(PREFIX)CAM1:ArrayCallbacks", 1)
+dbpf("$(PREFIX)MPEG1:EnableCallbacks", 1)
