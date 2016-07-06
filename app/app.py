20dbc74ba51f40efd4cd1925dc5d2ad79e654119
RobbieClarken <robbie.clarken@gmail.com>
2016-07-06 10:37:04 +1000

    Create pyepics detector device

--- app/app.py
+++ app/app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+import epics
+
+detector = epics.Device(prefix='SR00DEMO01:', mutable=False,
+                        aliases={
+                            'model': 'CAM1:Model_RBV',
+                            'video_url': 'MPEG1:MJPG_URL_RBV',
+                            'gain': 'CAM1:Gain',
+                            'acquire': 'CAM1:Acquire',
+                        })
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return detector.model
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
