3c8f720dc6f067aa9fafe368bf3718c3dc80057c
RobbieClarken <robbie.clarken@gmail.com>
2016-07-06 10:45:19 +1000

    Add a form to configure detector gain

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from time import sleep
+
+from flask import Flask, render_template, request
 import epics
 
 detector = epics.Device(prefix='SR00DEMO01:', mutable=False,
@@ -16,6 +18,14 @@ app = Flask(__name__)
 def index():
     video_url = detector.get('video_url', as_string=True)
     return render_template('index.html', detector=detector, video_url=video_url)
+
+
+@app.route('/configure', methods=['POST'])
+def configure():
+    data = request.form
+    detector.gain = float(data['gain'])
+    sleep(.1)
+    return 'configured!'
 
 
 if __name__ == '__main__':
